5936d7c04766eef5bcedb0ff0447dbec53a990a9
twmd <slava.sazanov@gmail.com>
2020-04-04 12:42:56 +0300

    add start.py file

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import subprocess
+import multiprocessing
+# def test():
+#     a = input('Enter message: ')
+#     print(a)
+arg = r'python C:\_slava\_work\Dropbox\code\Chat\chat_client\test2.py'
 
-proc_list = []
-args = ["ping", "www.yahoo.com"]
-proc_list.append(subprocess.Popen(args, creationflags=subprocess.CREATE_NEW_CONSOLE).communicate())
-print(proc_list)
+for _ in range (2):
+    subprocess.Popen(arg, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+# args = ["ping", "www.yahoo.com"]
+# for _ in range (5):
+#     subprocess.Popen(args, creationflags=subprocess.CREATE_NEW_CONSOLE)
+
